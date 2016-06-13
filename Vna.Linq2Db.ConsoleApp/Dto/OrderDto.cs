9daca3721e928766d624720b3091703c722ef593
Nicolay Velizhanin <inickvel@live.com>
2016-06-13 16:07:06 +0500

    Automapper dto

--- Vna.Linq2Db.ConsoleApp/Dto/OrderDto.cs
+++ Vna.Linq2Db.ConsoleApp/Dto/OrderDto.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Vna.Linq2Db.ConsoleApp.Dto
 {
     public class OrderDto
     {
         public int OrderID { get; set; }
-        public string CustomerContactName { get; set; }
-        public DateTime? OrderDate { get; set; }
+
+        public short Quantity { get; set; }
+
+        public string OrderDetailsOrderCustomerContactName { get; set; }
+
+        public DateTime? OrderDetailsOrderOrderDate { get; set; }
+
+        public string OrderDetailsProductProductName { get; set; }
+
+        [IgnoreMap]
+        public ICollection<int> BasinIds { get; set; }
     }
 }
--- Vna.Linq2Db.ConsoleApp/Program.cs
+++ Vna.Linq2Db.ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
-﻿using LinqToDB;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using LinqToDB;
 using LinqToDB.Data;
 using System;
 using System.Diagnostics;
 using System.Linq;
+using Vna.Linq2Db.ConsoleApp.Dto;
 using Vna.Linq2Db.DataModels;
 
 namespace Vna.Linq2Db.ConsoleApp
@@ -11,6 +14,8 @@ namespace Vna.Linq2Db.ConsoleApp
     {
         static void Main(string[] args)
         {
+            InitializeMapper();
+
             //LinqToDB.Common.Configuration.Linq.AllowMultipleQuery = true;
             DataConnection.TurnTraceSwitchOn();
             DataConnection.WriteTraceLine = (s, s1) => Debug.WriteLine(s, s1);
@@ -18,20 +23,32 @@ namespace Vna.Linq2Db.ConsoleApp
             using (var db = new NorthwindDb())
             {
                 var q =
-                    from o in db.Orders
-                        .LoadWith(o => o.Customer)
-                        .LoadWith(o => o.Employee)
-                    where o.Employee.LastName.StartsWith("King")
-                    select o;
+                    (from o in db.OrderDetails
+                        .LoadWith(o => o.OrderDetailsOrder)
+                        .LoadWith(o => o.OrderDetailsOrder.Customer)
+                        .LoadWith(o => o.OrderDetailsOrder.Employee)
+                     where o.OrderDetailsOrder.Employee.LastName.StartsWith("King")
+                     select o)
+                    .ProjectTo<OrderDto>()
+                    .ToList();
 
                 foreach (var o in q)
                 {
                     Console.WriteLine(
-                        $"{o.OrderDate} CustomerContactName: {o.Customer.ContactName} EmployeeLastName: {o.Employee.LastName}");
+                        $"OrderDate: {o.OrderDetailsOrderOrderDate}\tOrderDetailsProductProductName:{o.OrderDetailsProductProductName}\tCustomerContactName: {o.OrderDetailsOrderCustomerContactName}\tQuantity: {o.Quantity}");
                 }
             }
 
             Console.ReadLine();
+        }
+
+        private static void InitializeMapper()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<OrderDetail, OrderDto>();
+            });
+            Mapper.AssertConfigurationIsValid();
         }
     }
 }
